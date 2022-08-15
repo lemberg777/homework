7b2656ce9def714bbe1ffb6e5626e06db2209c2a
lemberg777 <yura.kuzyak@gmail.com>
2022-08-15 11:48:06 +0300

    add

---
+++ javascript/lesson1/index.js
@@ -0,0 +1,8 @@
+let n = 10;
+let sum = 0;
+let a = 1;
+while (n > 0) {
+  sum += a;
+  a += 1;
+}
+console.log("Result: " + sum);
---
+++ javascript/lesson4/tab-mnozhennja/index.js
@@ -0,0 +1,6 @@
+for (let a = 1; a <= 5; a++) {
+  console.log("\n=> " + a + " <=\n\n");
+  for (let b = 1; b <= 10; b++) {
+    console.log(a + " x " + b + " = " + a * b);
+  }
+}
